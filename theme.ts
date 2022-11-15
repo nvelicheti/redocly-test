49e27478b5913fb7b6771ba06f5b189934611423
nvelicheti <116834897+nvelicheti@users.noreply.github.com>
2022-11-15 16:09:26 -0500

    Update theme.ts

--- theme.ts
+++ theme.ts
@@ -16,7 +16,7 @@ export const theme = {
   colors: {
     tonalOffset: 0.2,
     primary: {
-      main: ({ colors }) => '#FFFFFF',
+      main: ({ colors }) => '#000000',
       light: ({ colors }) => lighten(colors.tonalOffset, colors.primary.main),
       dark: ({ colors }) => darken(colors.tonalOffset, colors.primary.main),
       contrastText: ({ colors }) => readableColor(colors.primary.main),
