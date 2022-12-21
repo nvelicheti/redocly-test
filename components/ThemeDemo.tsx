2df80bdd58aa3cc81e8addae0f9caf0f184a3052
John Shroff <John.Shroff@experian.com>
2022-12-21 11:40:31 -0600

    UNITY-922 add react poc to project

---
+++ components/ThemeDemo.tsx
@@ -0,0 +1,26 @@
+import React, { PropsWithChildren, useState } from 'react';
+import styled from 'styled-components';
+
+import { getThemeProps, ThemeSelector } from './ThemeSelector';
+
+const ThemeContainer = styled.div<{ $inFrame?: boolean }>`
+  margin: 16px 0 0 16px;
+`;
+
+const CreditScoreModule = ({theme}) => (<credit-score-module size={"small"} token={"abc123"} {...theme} />);
+
+export const ThemeDemo = ({
+}: PropsWithChildren<{ inProgress?: boolean; inFrame?: boolean }>) => {
+  const [theme, setTheme] = useState<string>('default');
+
+  const handleThemeChange = (t: string) => {
+    setTheme(t);
+  };
+
+  return (
+    <ThemeContainer>
+      <CreditScoreModule {...getThemeProps(theme)} />
+      <ThemeSelector onChange={handleThemeChange} />
+    </ThemeContainer>
+  );
+};
