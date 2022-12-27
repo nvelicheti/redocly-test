467409d3981e395fd62bbc1dec15bb3de2362167
nvelicheti <116834897+nvelicheti@users.noreply.github.com>
2022-12-27 11:22:56 -0500

    Create tryit.tsx

---
+++ _override/tryit.tsx
@@ -0,0 +1,15 @@
+import { setParameterValue, setSecurityDetails, getUserClaims } from '@redocly/developer-portal/ui';
+
+export function onInit() {
+  const claims = getUserClaims(); // you can use user claims if login is enabled or get value from other place
+
+  // setParameterValue(in, name, value)
+  setParameterValue('path', 'petId', 25);
+  setParameterValue('header', 'x-user-email', claims?.email);
+
+  setSecurityDetails('api_key', 'sk_123123'); // 'api_key' is the security scheme id from the OAS definition
+  setSecurityDetails('my_oauth2', {
+    client_id: 'user1',
+    client_secret: 'secret123'
+  });
+}
