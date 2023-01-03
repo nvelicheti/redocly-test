9fcd06a26bbe6a4754662bf6dcfd138d3bf4fc77
nvelicheti <116834897+nvelicheti@users.noreply.github.com>
2023-01-03 13:56:55 -0500

    Update ReferenceDocsHooks.tsx

--- _override/ReferenceDocsHooks.tsx
+++ _override/ReferenceDocsHooks.tsx
@@ -5,7 +5,7 @@ export function onInit() {
 
   // setParameterValue(in, name, value)
   setParameterValue('path', 'petId', 25);
-  setParameterValue('header', 'x-user-email', claims?.email);
+  setParameterValue('header', 'x-user-email', 'email@email.com');
     setParameterValue('header', 'Authorization', 'Bearer '+ 'blah blah');
 
 
