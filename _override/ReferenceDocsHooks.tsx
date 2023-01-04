e34c63c4d0e33cd6fb65147a88dbaaf961aca10b
nvelicheti <116834897+nvelicheti@users.noreply.github.com>
2023-01-04 13:32:36 -0500

    Update ReferenceDocsHooks.tsx

--- _override/ReferenceDocsHooks.tsx
+++ _override/ReferenceDocsHooks.tsx
@@ -4,9 +4,9 @@ export function onInit() {
   const claims = getUserClaims(); // you can use user claims if login is enabled or get value from other place
 
   // setParameterValue(in, name, value)
-  setParameterValue('path', 'petId', 25);
-  setParameterValue('header', 'x-user-email', 'email@email.com');
-    //setParameterValue('header', 'Authorization', 'Bearer:'+ 'blah blah');
+  setParameterValue('header', 'X-Session-ID', 25);
+  setParameterValue('header', 'X-Correlation-ID', 'email@email.com');
+
 
 
  
